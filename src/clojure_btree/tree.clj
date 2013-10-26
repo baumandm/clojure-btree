6c828dcb19fbb87627e071f9aa7d247ebb274467
Dave Bauman <baumandm@gmail.com>
2013-10-26 11:50:58 -0700

    Add a bunch of methods and tests

--- src/clojure_btree/tree.clj
+++ src/clojure_btree/tree.clj
@@ -19,6 +19,11 @@
         (reduce add-value-to-node (add-value-to-node node value) more))
 )
 
+(defn make-tree
+    "Creates a tree with one or more values, added in order"
+    [& values]
+    (apply add-value-to-node nil values))
+
 (defn count-nodes
     "Counts all the child nodes of the argument, including itself"
     [node]
@@ -42,3 +47,84 @@
         0
         (count-nodes (:right node))
     ))
+
+(defn flatten-nodes-inorder
+    "Flattens a tree into a list in order"
+    [node]
+    (if (nil? node)
+        '()
+        (concat 
+            (flatten-nodes-inorder (:left node)) 
+            (list (:value node)) 
+            (flatten-nodes-inorder (:right node)))))
+
+(defn tree-depth
+    "Calculates the depth of the tree, starting at 1 for the root node"
+    [node]
+    (if (nil? node)
+        0
+        (+ 1 (max (tree-depth (:left node))
+                  (tree-depth (:right node))))))
+
+(defn find-node
+    "Returns the node with the given value"
+    [node value]
+    (if (nil? node) 
+        nil
+        (let [current-value (:value node)]
+            (cond 
+                (< value current-value) (find-node (:left node) value)
+                (> value current-value) (find-node (:right node) value)
+                :else node
+            ))))    
+
+(defn find-min-node
+    "Returns the node with the smallest value in the tree"
+    [node]
+    (if (nil? node)
+        nil
+        (if-let [left (:left node)]
+            (find-min-node left)
+            node)))
+
+(defn find-max-node
+    "Returns the node with the largest value in the tree"
+    [node]
+    (if (nil? node)
+        nil
+        (if-let [right (:right node)]
+            (find-max-node right)
+            node)))
+
+(defn find-min-value
+    "Returns the smallest value in the tree"
+    [node]
+    (if-let [smallest-node (find-min-node node)]
+        (:value smallest-node)
+        nil))    
+            
+(defn find-max-value
+    "Returns the largest value in the tree"
+    [node]
+    (if-let [largest-node (find-max-node node)]
+        (:value largest-node)
+        nil))
+
+(defn remove-value
+    "Removes a value from the tree"
+    [node value]
+    (let [current-value (:value node)]
+        (cond
+            (nil? node) nil
+            (< value current-value) (assoc node :left (remove-value (:left node) value))
+            (> value current-value) (assoc node :right (remove-value (:right node) value))
+            :else (let [left (:left node)
+                        right (:right node)]
+                (cond
+                    (and (nil? left) (nil? right)) nil ; remove current node
+                    (nil? left) right ; replace current with right
+                    (nil? right) left ; replace current with left
+                    :else (let [smallest-value (find-min-value right)]
+                        (assoc node :value smallest-value
+                                    :right (remove-value right smallest-value)))
+            )))))
--- test/clojure_btree/tree_test.clj
+++ test/clojure_btree/tree_test.clj
@@ -8,6 +8,11 @@
             (->Node 99 nil nil)
             (add-value-to-node nil 99))))
 
+    (testing "with same value"
+        (is (=
+            (->Node 99 nil nil)
+            (add-value-to-node nil 99 99))))
+
     (testing "with a node that has a larger value"
         (is (= 
             (->Node 100 (->Node 99 nil nil) nil)
@@ -39,6 +44,14 @@
             (add-value-to-node (->Node 101 nil nil) 106 102 100 99))))
 )
 
+(deftest test-make-tree
+    (testing "with nil"
+        (is (->Node nil nil nil) (make-tree nil)))
+    (testing "with one value"
+        (is (->Node 6 nil nil) (make-tree 6)))
+    (testing "with two values"
+        (is (->Node 6 nil (->Node 7 nil nil)) (make-tree 6 7))))
+
 (deftest test-count-nodes
     (testing "with empty tree"
         (is (= 0 (count-nodes nil))))
@@ -62,3 +75,105 @@
 
     (testing "with 4 nodes"
         (is (= 3 (count-nodes-on-right (add-value-to-node nil 1 2 3 4))))))
+
+(deftest test-flatten-nodes-inorder
+    (testing "with nil"
+        (is (= '() (flatten-nodes-inorder nil))))
+    (testing "with single node"
+        (is (= '(1) (flatten-nodes-inorder (->Node 1 nil nil)))))
+    (testing "with two nodes in order"
+        (is (= '(1 2) (flatten-nodes-inorder (add-value-to-node nil 1 2)))))
+    (testing "with two nodes out of order"
+        (is (= '(1 2) (flatten-nodes-inorder (add-value-to-node nil 2 1)))))
+    (testing "with three nodes unbalanced"
+        (is (= '(1 2 3) (flatten-nodes-inorder (add-value-to-node nil 1 2 3)))))
+    (testing "with three nodes balanced"
+        (is (= '(1 2 3) (flatten-nodes-inorder (add-value-to-node nil 2 1 3)))))
+    (testing "with many nodes"
+        (is (= '(1 2 3 4 5 6 9) (flatten-nodes-inorder (make-tree 1 6 4 5 9 3 2)))))
+    )
+
+(deftest test-tree-depth
+    (testing "with nil"
+        (is (= 0 (tree-depth nil))))
+    (testing "with single node"
+        (is (= 1 (tree-depth (->Node 1 nil nil)))))
+    (testing "with two nodes in order"
+        (is (= 2 (tree-depth (add-value-to-node nil 1 2)))))
+    (testing "with two nodes out of order"
+        (is (= 2 (tree-depth (add-value-to-node nil 2 1)))))
+    (testing "with three nodes unbalanced"
+        (is (= 3 (tree-depth (add-value-to-node nil 1 2 3)))))
+    (testing "with three nodes balanced"
+        (is (= 2 (tree-depth (add-value-to-node nil 2 1 3)))))
+    (testing "with many nodes"
+        (is (= 5 (tree-depth (make-tree 1 6 4 5 3 2)))))
+    )
+
+(deftest test-find-node
+    (testing "with nil"
+        (is (nil? (find-node nil 1))))
+    (testing "with one node"
+        (is (= (->Node 1 nil nil) (find-node (make-tree 1) 1))))
+    (testing "with one node that doesn't match"
+        (is (nil? (find-node (make-tree 1) 5))))
+    (testing "with two nodes"
+        (is (= (->Node 5 nil nil) (find-node (make-tree 1 5) 5))))
+    (testing "with three nodes"
+        (is (= (->Node 1 nil nil) (find-node (make-tree 3 5 1) 1))))
+    (testing "with many nodes"
+        (is (= (->Node 3 (->Node 1 nil (->Node 2 nil nil)) (->Node 4 nil nil)) (find-node (make-tree 5 10 6 5 3 4 9 1 7 2) 3))))
+    )
+
+(deftest test-find-min-node
+    (testing "with nil"
+        (is (nil? (find-min-node nil))))
+    (testing "with one node"
+        (is (= (->Node 10 nil nil) (find-min-node (make-tree 10)))))
+    (testing "with many nodes"
+        (is (= (make-tree 1 2) (find-min-node (make-tree 5 10 6 5 3 4 9 1 7 2)))))
+    )
+
+(deftest test-find-min-value
+    (testing "with nil"
+        (is (nil? (find-min-value nil))))
+    (testing "with one node"
+        (is (= 10 (find-min-value (make-tree 10)))))
+    (testing "with many nodes"
+        (is (= 1 (find-min-value (make-tree 5 10 6 5 3 4 9 1 7 2)))))
+    )
+
+(deftest test-find-max-value
+    (testing "with nil"
+        (is (nil? (find-max-value nil))))
+    (testing "with one node"
+        (is (= 10 (find-max-value (make-tree 10)))))
+    (testing "with many nodes"
+        (is (= 10 (find-max-value (make-tree 5 10 6 5 3 4 9 1 7 2)))))
+    )
+
+(deftest test-remove-value
+    (testing "with nil"
+        (is (nil? (remove-value nil 1))))
+    (testing "with one value"
+        (is (nil? (remove-value (make-tree 1) 1))))
+    (testing "with unknown value"
+        (is (= (->Node 1 nil nil) (remove-value (make-tree 1) 2))))
+    (testing "with remove left"
+        (is (= (->Node 2 nil nil) (remove-value (make-tree 2 1) 1))))
+    (testing "with remove right"
+        (is (= (->Node 2 (->Node 1 nil nil) nil) (remove-value (make-tree 2 1 3) 3))))
+    (testing "with remove balanced"
+        (is (= (->Node 3 (->Node 1 nil nil) nil) (remove-value (make-tree 2 1 3) 2))))
+    (testing "with remove complex"
+        (is (= (make-tree 5 10 6 9 7 4 1 2) (remove-value (make-tree 5 10 6 5 3 4 9 1 7 2) 3))))
+    (testing "with remove complex-2"
+        (is (= (make-tree 5 10 6 9 7 3 1 2) (remove-value (make-tree 5 10 6 5 3 4 9 1 7 2) 4))))
+    (testing "with remove complex-3"
+        (is (= (make-tree 5 10 6 9 7 3 4 2) (remove-value (make-tree 5 10 6 5 3 4 9 1 7 2) 1))))
+    (testing "with remove complex-4"
+        (is (= (make-tree 6 10 9 7 3 1 4 2) (remove-value (make-tree 5 10 6 5 3 4 9 1 7 2) 5))))
+    (testing "with remove multiple"
+        (is (= (make-tree 7 10 9 3 1 4 2) 
+               (remove-value (remove-value (make-tree 5 10 6 5 3 4 9 1 7 2) 5) 6))))
+    )
